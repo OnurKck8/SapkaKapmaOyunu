64f112ebd8310eb62fa469ebd26d6ec83f8c8d19
Onur Küçük <124556360+OnurKck8@users.noreply.github.com>
2023-09-14 11:05:14 +0300

    Added Winner Panel and ...
    Game Manager Script added
    Spawn Player added
    Give Hat added

---
+++ SapkaKapma3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")]
+    public bool gameEnded = false;
+    public float timeToWin;
+    public float invictibleDuration;
+
+    private float hatPickupTÝme;//þapka tutma sürelerini tutmak için
+
+    [Header("Player")]
+    public string playerPrefabLocation;//kullnýcýnýn prefabýnýn adresini tutmak için. Oluþacak obje
+
+    public Transform[] spawnPoints;//çýkma yerleri için
+    public PlayerController[] players;//kaç tane oyunucu var tutmak için
+    public int playerWithHat;//þapkalý oyuncunun idsi
+    private int playersInGame;//oyunda kaç kiþi var.
+
+    public static GameManager Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];//playerlistin uzunluðu kadar.
+        photonView.RPC("ImInGame", RpcTarget.All);
+    }
+
+    [PunRPC]
+    void WinGame(int playerId)
+    {
+        gameEnded = true;
+        PlayerController player = GetPlayer(playerId);
+        Invoke("GoBackToMenu",3.0f);
+    }
+
+    void GoBackToMenu()
+    {
+        PhotonNetwork.LeaveRoom();
+        NetworkManager.Instance.ChangeScene("MainMenu");
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    [PunRPC]
+    public void GiveHat(int playerId,bool initialGive)
+    {
+        if(!initialGive)//þapka verilmediyse
+        {
+            GetPlayer(playerWithHat).SetHat(false);
+        }
+        playerWithHat = playerId;
+        GetPlayer(playerId).SetHat(true);
+        hatPickupTÝme = Time.time;//alýnca sayac baþlasýn
+    }
+
+    public bool CanHetHat()
+    {
+        if(Time.time > hatPickupTÝme + invictibleDuration)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, 
+            spawnPoints.Length)].position, 
+            Quaternion.identity);
+
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+
+        playerScript.photonView.RPC("Initialized", RpcTarget.All,PhotonNetwork.LocalPlayer);
+    }
+
+    public PlayerController GetPlayer(int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+
+    public PlayerController GetPlayer(GameObject playerObj)
+    {
+        return players.First(x => x.gameObject == playerObj);
+    }
+
+}
--- SapkaKapma3D/Assets/Scripts/PlayerController.cs
+++ SapkaKapma3D/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks,IPunObservable
 {
     [HideInInspector]
     public int id;//herkese id bildirmek için public yaptý. Ama deðiþtirmemek için gizledi.
@@ -22,11 +22,29 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Move();
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(PhotonNetwork.IsMasterClient)
         {
-            TryJump();
+            //þapkalý süre kazanma süresini geçtiyse ve oyun bitmediyse
+            if(curHatTime>=GameManager.Instance.timeToWin && !GameManager.Instance.gameEnded)
+            {
+                GameManager.Instance.gameEnded = true;
+                GameManager.Instance.photonView.RPC("WinGame", RpcTarget.All,id);
+            }
         }
+
+        if(photonView.IsMine)
+        {
+            Move();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TryJump();
+            }
+
+            if(hatObject.activeInHierarchy)//þapka oluþtuðu sürece
+            {
+                curHatTime += Time.deltaTime;//durmadan saymak için
+            }
+        } 
     }
 
     public void Move()
@@ -45,6 +63,60 @@ public class PlayerController : MonoBehaviour
         if(Physics.Raycast(ray,0.7f))//zeminden 0.7 yukarýda ise 
         {
             rb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);//hafif silkelesin force modu
+        }
+    }
+
+    public void SetHat(bool hasHat)
+    {
+        hatObject.SetActive(hasHat);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (photonView.IsMine)//þapka bende deðilse bir þey yapma diðerleri çarpýþýnca
+            return;
+
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if(GameManager.Instance.GetPlayer(collision.gameObject).id==GameManager.Instance.playerWithHat)//Benim çarptýðým kiþinin id'si withhat'in idsine eþitse
+            {
+                if(GameManager.Instance.CanHetHat())// alacabilecek durumda ise
+                {
+                    GameManager.Instance.photonView.RPC("GiveHat", RpcTarget.All,id,false);
+                }
+            }
+        }
+    }
+
+    [PunRPC]
+    public void Initialized(Player player)
+    {
+        photonPlayer = player;
+        id = player.ActorNumber;//oyuncunun kendi idsini verir.
+
+        GameManager.Instance.players[id - 1]=this;
+
+        if (id == 1)
+            GameManager.Instance.GiveHat(id, true);//en baþta birinde þapka olmasý için
+
+        //ben deðilsem diðerlerini true yapar kendinikini oynatýr tek
+        if (!photonView.IsMine)
+        {
+            rb.isKinematic = true;
+        }
+
+    }
+
+    //zaman bilgisinin sekronize çalýþmasý için
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(curHatTime);//bilgiyi sen veriyorsan gönder
+        }
+        else if(stream.IsReading)
+        {
+            curHatTime =(float) stream.ReceiveNext();//bilgiyi sen alýyorsan oradan oku
         }
     }
 }
